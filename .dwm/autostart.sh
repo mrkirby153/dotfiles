d7c086507a98879a2fe22197c0e5c31d68b77a2d
mrkirby153 <mr.austinwhyte@gmail.com>
2023-07-13 22:02:23 -0700

    Update startup script

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -3,8 +3,10 @@
 
 pidof DiscordCanary || setsid -f discord-canary --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy
 
-pidof spotify || setsid -f spotify
+pidof spotify || setsid -f spotify-launcher
 
 pidof 1password || setsid -f /sbin/1password --silent
 numlockx
+
+setxkbmap -option compose:ralt
 
