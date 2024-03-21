5e64beb612f80a3a99557ccaf5aea8ab689da867
mrkirby153 <mr.austinwhyte@gmail.com>
2024-03-20 23:09:18 -0700

    Automatically update nix-index

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -6,6 +6,19 @@
 }: let
   httpServer = pkgs.python3.withPackages (ps: with ps; [httpserver]);
   notifier = pkgs.writeShellScriptBin "notifier" (builtins.readFile ./notifier);
+  shellScript = import ../../lib/shellScript.nix {
+    inherit (pkgs) writeTextFile runtimeShell;
+    inherit pkgs lib;
+  };
+  nix_index_updater = shellScript {
+    name = "nix-index-updater";
+    path = ./nix_index_update;
+    deps = with pkgs; [
+      jq
+      curl
+      coreutils
+    ];
+  };
 in {
   options.aus = {
     programs.shell = {
@@ -119,5 +132,31 @@ in {
       "$HOME/.local/bin"
       "$HOME/.mix/escripts"
     ];
+
+    systemd.user.services = {
+      "nix-index-update" = {
+        Unit = {
+          Description = "Updates the nix-index";
+        };
+        Service = {
+          Type = "oneshot";
+          ExecStart = "${nix_index_updater}/bin/nix-index-updater";
+        };
+      };
+    };
+
+    systemd.user.timers = {
+      "nix-index-update" = {
+        Unit = {
+          Description = "Updates the nix-index";
+        };
+        Timer = {
+          OnCalendar = "Sun *-*-* 00:00:00";
+          Persistent = true;
+          Unit = "nix-index-update.service";
+        };
+        Install.WantedBy = ["timers.target"];
+      };
+    };
   };
 }
