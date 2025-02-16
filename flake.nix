3b37725430ef009b291a9625662767f4ea66482e
mrkirby153 <mr.austinwhyte@gmail.com>
2025-02-16 14:13:51 -0800

    Use the right branch of nix darwin

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:LnL7/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
   };
